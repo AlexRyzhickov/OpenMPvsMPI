6b2d60bc7c18a72ddf8b37381eb50481466d47e8
AlexRyzhickov <makseljoinb@gmail.com>
2022-11-14 17:18:25 +0300

    new done 00

--- 00/open_mp.cpp
+++ 00/open_mp.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <omp.h>
 int main() {
-    int counts_threads = 0;
-#pragma omp parallel
-    {
-        if (omp_get_thread_num() == 0) {
-            counts_threads = omp_get_num_threads();
-        }
-    }
+    int counts_threads = omp_get_max_threads();
     int *arr = new int[counts_threads];
+    omp_lock_t *mutexes = new omp_lock_t[counts_threads];
+    for (int i = 0; i < counts_threads; ++i) {
+        omp_init_lock(&mutexes[i]);
+        omp_set_lock(&mutexes[i]);
+    }
+    omp_unset_lock(&mutexes[0]);
 #pragma omp parallel
     {
-        arr[omp_get_thread_num()] = omp_get_thread_num();
-    }
-    for (int i = 0; i < counts_threads; ++i) {
-        printf("Hello from process  %d\n", arr[i]);
+        int id = omp_get_thread_num();
+        omp_set_lock(&mutexes[id]);
+        arr[id] = id;
+        printf("Hello from process %d\n", arr[id]);
+        omp_unset_lock(&mutexes[id + 1]);
     }
     return 0;
 }
