3c692ea0e1666b8241111c33e2b4e90f9fcf8a97
AlexRyzhickov <makseljoinb@gmail.com>
2022-11-14 14:32:26 +0300

    done 00

--- 00/open_mp.cpp
+++ 00/open_mp.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <omp.h>
 int main() {
-    printf("number %d\n", omp_get_num_threads());
+    int counts_threads = 0;
 #pragma omp parallel
     {
-        printf("Thread %d from %d says \"Hello World!\"\n", omp_get_thread_num(), omp_get_num_threads());
+        if (omp_get_thread_num() == 0) {
+            counts_threads = omp_get_num_threads();
+        }
     }
-    printf("Finished.\n");
-//    std::cout << "Hello, World!" << std::endl;
+    int *arr = new int[counts_threads];
+#pragma omp parallel
+    {
+        arr[omp_get_thread_num()] = omp_get_thread_num();
+    }
+    for (int i = 0; i < counts_threads; ++i) {
+        printf("Hello from process  %d\n", arr[i]);
+    }
     return 0;
 }
